d7bbca0ed75b11efe5564f1468f0ef5b2654229b
TiagoViniciusF <tiago_vinicius_ferreira@hotmail.com>
2022-12-21 23:57:07 -0300

    implementando validações via javax.validation em UserDto

--- authuser/src/main/java/com/ead/authuser/dto/UserDto.java
+++ authuser/src/main/java/com/ead/authuser/dto/UserDto.java
@@ -6,6 +6,7 @@ import lombok.Data;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
@@ -20,20 +21,23 @@ public class UserDto {
     }
 
     private UUID uderId;
-    @NotBlank
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Size(min = 4, max = 50)
     @JsonView(UserView.RegistrationPost.class)
     private String username;
-
-    @NotBlank
     @Email
+    @NotBlank(groups = UserView.RegistrationPost.class)
+    @Size(min = 4, max = 50)
     @JsonView(UserView.RegistrationPost.class)
     private String email;
 
-    @NotBlank
+    @NotBlank(groups = {UserView.RegistrationPost.class,  UserView.PasswordPut.class})
+    @Size(min = 6, max = 20)
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
 
-    @NotBlank
+    @NotBlank(groups = UserView.PasswordPut.class)
+    @Size(min = 6, max = 20)
     @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
 
@@ -46,7 +50,7 @@ public class UserDto {
     @JsonView({UserView.RegistrationPost.class, UserView.UserPut.class})
     private String cpf;
 
-    @NotBlank
+    @NotBlank(groups = UserView.ImagePut.class)
     @JsonView(UserView.ImagePut.class)
     private String imageUrl;
 
