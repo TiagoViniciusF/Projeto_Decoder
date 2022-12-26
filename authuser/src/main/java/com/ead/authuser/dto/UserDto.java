a96ee96f754dd80104040789c8bf680d1e3a69ff
TiagoViniciusF <tiago_vinicius_ferreira@hotmail.com>
2022-12-26 20:05:34 -0300

    criando anotação UsernameConstraint

--- authuser/src/main/java/com/ead/authuser/dto/UserDto.java
+++ authuser/src/main/java/com/ead/authuser/dto/UserDto.java
@@ -1,5 +1,6 @@
 package com.ead.authuser.dto;
 
+import com.ead.authuser.validation.UsernameConstraint;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.Data;
@@ -13,6 +14,8 @@ import java.util.UUID;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class UserDto {
 
+    //criando grupos visualizações para os campos nos endpoints de PUT e POST.
+    // Alguns campos só serão visiveis em determinados grupos
     public interface UserView{
         public static interface RegistrationPost{}
         public static interface UserPut{}
@@ -21,23 +24,26 @@ public class UserDto {
     }
 
     private UUID uderId;
+
+    //Definindo anotações e grupos
     @NotBlank(groups = UserView.RegistrationPost.class)
-    @Size(min = 4, max = 50)
+    @Size(min = 4, max = 50, groups = UserView.RegistrationPost.class)
+    @UsernameConstraint(groups = UserView.RegistrationPost.class)
     @JsonView(UserView.RegistrationPost.class)
     private String username;
-    @Email
+    @Email(groups = UserView.RegistrationPost.class)
     @NotBlank(groups = UserView.RegistrationPost.class)
-    @Size(min = 4, max = 50)
+    @Size(min = 4, max = 50, groups = UserView.RegistrationPost.class)
     @JsonView(UserView.RegistrationPost.class)
     private String email;
 
     @NotBlank(groups = {UserView.RegistrationPost.class,  UserView.PasswordPut.class})
-    @Size(min = 6, max = 20)
+    @Size(min = 6, max = 20, groups = {UserView.RegistrationPost.class,  UserView.PasswordPut.class})
     @JsonView({UserView.RegistrationPost.class, UserView.PasswordPut.class})
     private String password;
 
     @NotBlank(groups = UserView.PasswordPut.class)
-    @Size(min = 6, max = 20)
+    @Size(min = 6, max = 20, groups = UserView.PasswordPut.class)
     @JsonView(UserView.PasswordPut.class)
     private String oldPassword;
 
