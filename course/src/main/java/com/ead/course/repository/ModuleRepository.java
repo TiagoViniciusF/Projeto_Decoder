90b259b4ff58ee520a544ce51d03eb2e18271445
TiagoViniciusF <tiago_vinicius_ferreira@hotmail.com>
2023-01-04 21:53:44 -0300

    Criando metodo de delete em cascata para course, Module e Lesson

--- course/src/main/java/com/ead/course/repository/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repository/ModuleRepository.java
@@ -12,7 +12,7 @@ import java.util.UUID;
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
 
     @Query(value="select * from tb_modules where course_course_id = :courseId", nativeQuery = true)
-    List<ModuleModel> findAllModulesCourse(@Param("courseId") UUID courseId);
+    List<ModuleModel> findAllModulesIntoCourse(@Param("courseId") UUID courseId);
 
     //@EntityGraph = Traz atributo que está setado com FetchType.LAZY, como FetchType.EAGER
 //    @EntityGraph(attributePaths = {"course"})
